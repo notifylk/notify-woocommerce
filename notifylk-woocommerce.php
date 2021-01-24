7b9b9ffed969846c0f1830243b4fde5bc87fba37
Praboda Ariyasinghe <maduonline@users.noreply.github.com>
2021-01-24 13:32:09 +0530

    improve plugin details.

--- notifylk-woocommerce.php
+++ notifylk-woocommerce.php
@@ -1,13 +1,14 @@
 <?php
 
 /**
- * Plugin Name:       NotifyLK SMS for WooCommerce
- * Description:       Send WooCommerce order updates to customers with Notify.LK SMS Gateway.
+ * Plugin Name:       Notify.lk SMS for WooCommerce
+ * Description:       Send WooCommerce order updates to customers with Notify.lk SMS Gateway. 
  * Version:           1.0.0
  * Author:            Notify.lk
- * Author URI:        http://notify.lk/
- * License:           GPLv2 or laterx
+ * Author URI:        https://www.notify.lk/
+ * License:           GPLv2 or later
  * Text Domain:       notifylk
  */
+
 include 'includes/core-import.php';
 new NotifyLKSMS(__FILE__);
